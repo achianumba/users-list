e2a2be21d9f7907550f2f78b715e2eb849293eef
achianumba <chianumbaarinze@gmail.com>
2022-12-06 23:22:26 +0100

    adds partial mobile styling, especially mobile nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import './App.css';
 import searchIcon from './images/icon-search.svg';
@@ -7,15 +7,38 @@ import menuIcon from './images/icon-menu.svg';
 import NavItem from './components/NavItem/NavItem';
 import usersArray from './data/users';
 import User from './components/User/User';
+import closeIcon from './images/icon-close.svg';
 
 function App() {
   // eslint-disable-next-line
   const [users, setUsers] = useState(usersArray);
+  const [searchQuery, setSearchQuery] = useState('');
+  const navbarRef = useRef(null);
+
+  const searchForUser = (e) => {
+    setSearchQuery(e.target.value);
+    const match = users.filter(
+      (user) => !!searchQuery && user.name.includes(searchQuery)
+    );
+
+    console.log(match);
+  };
+
+  const toggleNavbar = () => {
+    navbarRef.current?.classList.toggle('page__nav__toggle');
+    console.log(navbarRef.current?.classList);
+  };
+
   return (
     <BrowserRouter>
       <header className="page__header">
-        <h1 className="page__heading">Users</h1>
-
+        <h1 className="page__heading">Users</h1>{' '}
+        <Icon
+          src={menuIcon}
+          alt="menu"
+          className="page__menu__icon"
+          onClick={toggleNavbar}
+        />
         <div className="page__nav__and__search">
           <div className="page__search__form">
             <Icon
@@ -28,18 +51,27 @@ function App() {
               type="search"
               placeholder="Search users"
               className="page__search__input"
+              onChange={searchForUser}
+              value={searchQuery}
             />
           </div>
 
-          <nav className="page__nav">
-            <NavItem to="/users/reputation">Reputation</NavItem>
-            <NavItem to="/users/new">New users</NavItem>
-            <NavItem to="/users/voters">Voters</NavItem>
-            <NavItem to="/users/editors">Editors</NavItem>
-            <NavItem to="/users/moderators">Moderators</NavItem>
-          </nav>
+          <div className="page__nav__wrapper" ref={navbarRef}>
+            <Icon
+              src={closeIcon}
+              alt="close"
+              className="page__nav__close-icon"
+              onClick={toggleNavbar}
+            />
 
-          <Icon src={menuIcon} alt="menu" className="page__menu__icon" />
+            <nav className="page__nav">
+              <NavItem to="/users/reputation">Reputation</NavItem>
+              <NavItem to="/users/new">New users</NavItem>
+              <NavItem to="/users/voters">Voters</NavItem>
+              <NavItem to="/users/editors">Editors</NavItem>
+              <NavItem to="/users/moderators">Moderators</NavItem>
+            </nav>
+          </div>
         </div>
       </header>
 
